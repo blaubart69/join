5c89e35e1d915049e817199812c8f87f59dc7cb7
Bernhard Spindler <bernhard.spindler@unicredit.at>
2019-07-26 16:09:02 +0200

    do geht einiges!

---
+++ Misc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace join
+{
+    class Misc
+    {
+        public static IEnumerable<string[]> ReadTsv(TextReader tr)
+        {
+            return ReadLines(tr).Select(line => line.Split('\t'));
+        }
+        public static IEnumerable<string> ReadLines(TextReader reader)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+        public static IEnumerable<T> ForEachRow<T>(SQLiteConnection OpenedConnection, string SqlCommand, SQLiteParameter[] parameters, Func<SQLiteDataReader, T> Converter)
+        {
+            using (SQLiteCommand SqlCmd = new SQLiteCommand(SqlCommand))
+            {
+                SqlCmd.Connection = OpenedConnection;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    SqlCmd.Parameters.AddRange(parameters);
+                }
+
+                using (SQLiteDataReader rdr = SqlCmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        yield return Converter(rdr);
+                    }
+                }
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.SQLite;
+using System.IO;
 
 namespace join
 {
@@ -14,17 +15,82 @@ namespace join
 
         static void Main(string[] args)
         {
-            log.Info("start");
-            string DNSdomain = "eap.dev.lan.at";
+            string tmpDbName = "joinTemp.db";
 
-            foreach (var i in ADQuery.Filter(DNSdomain, ADQuery.DnsDomainToLdapDomain(DNSdomain), "(objectClass=computer)", null, new string[] {"pwdLastSet" }))
+            SQLiteConnection.CreateFile(tmpDbName);
+            SQLiteConnection db = new SQLiteConnection($"Data Source={tmpDbName};Version=3;");
+            db.Open();
+
+            var insertPwd = Task.Run(() =>
+           {
+               //new SQLiteCommand("create table pwd (hostname varchar(32), pwdLastSet varchar(32))", db).ExecuteNonQuery();
+               new SQLiteCommand("create table pwd (hostname text, pwdLastSet text)", db).ExecuteNonQuery();
+               long rows = InsertIntoTable(db, "pwd", Misc.ReadTsv(new StreamReader(@"u:\spindi\_werkbank\join\pwdLastSet.tsv")));
+               log.Debug($"pwd inserted: {rows}");
+           });
+
+            var insertEudb = Task.Run(() =>
             {
-                long pwdlastset = (long)i.Properties["pwdLastSet"][0];
-                var name = i.Properties["Name"].Value;
-                log.Info($"{name}\t{pwdlastset}");
+                new SQLiteCommand("create table eurodb (BuildingID	varchar(32), city varchar(32),	street varchar(32),	Site varchar(32),	Hostname varchar(32),	BootFlag varchar(32),	ProductsToInstall varchar(32),	ReleasesToInstall varchar(32),	Status varchar(32),	LastAliveDateTime varchar(32))", db).ExecuteNonQuery();
+                long rows = InsertIntoTable(db, "eurodb", Misc.ReadTsv(new StreamReader(@"u:\spindi\_werkbank\join\hd04machs.tsv")));
+                log.Debug($"eurodb inserted: {rows}");
+            });
 
+            Task.WaitAll(insertPwd, insertEudb);
+
+            string sql =
+                @"select date(p.pwdLastSet) as pwdLastSet, e.* 
+                from eurodb e join pwd p on p.hostname = e.Hostname";
+
+            StringBuilder line = new StringBuilder();
+            bool headerPrinted = false;
+            foreach ( var row in Misc.ForEachRow(db, sql, null, rdr => rdr))
+            {
+                if (!headerPrinted)
+                {
+                    headerPrinted = true;
+                    for (int i = 0; i < row.FieldCount; ++i)
+                    {
+                        line.Append(row.GetName(i));
+                        line.Append("\t");
+                    }
+                    line.Length -= 1;
+                    Console.WriteLine(line.ToString());
+                }
+
+                line.Length = 0;
+                for (int i=0; i < row.FieldCount; ++i)
+                {
+                    line.Append(row[i] as string);
+                    line.Append("\t");
+                }
+                if ( line.Length>0) line.Length -= 1;
+                Console.WriteLine(line.ToString());
             }
+
+            db.Close();
         }
+        private static long InsertIntoTable(SQLiteConnection db, string tablename, IEnumerable<string[]> enumerable)
+        {
+            long rowsInserted = 0;
+
+            using (var cmd = new SQLiteCommand() { Connection = db })
+            using (SQLiteTransaction trans = db.BeginTransaction())
+            {
+                foreach (string[] vals in enumerable)
+                {
+                    string valString = String.Join(",", vals.Select(i => $"'{i}'"));
+                    string stmt = $"insert into {tablename} values ({valString})";
+                    log.Debug(stmt);
+                    cmd.CommandText = stmt;
+                    cmd.ExecuteNonQuery();
+                    ++rowsInserted;
+                }
+                trans.Commit();
+            }
+            return rowsInserted;
+        }
+
         static SQLiteConnection CreateAndConnect(string tmpDatabaseFile)
         {
             SQLiteConnection.CreateFile(tmpDatabaseFile);
